684be9b89e47a5f269b5edf15c953cb3c86ce034
KostiaSA <savchenkov@buhta.ru>
2015-12-12 23:49:18 +0300

    app-nav-bar

---
+++ Buhta4/Areas/BuhtaCore/Models/AppNavBarModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Buhta
+{
+    public class AppNavBarModel : BaseModel
+    {
+        public Guid UserID;
+
+        public AppNavBarModel(Controller controller, BaseModel parentModel) : base(controller, parentModel) { }
+
+        bool okClicked;
+
+        public virtual void CancelButtonClick(dynamic args)
+        {
+            Modal.Close();
+        }
+
+    }
+}
