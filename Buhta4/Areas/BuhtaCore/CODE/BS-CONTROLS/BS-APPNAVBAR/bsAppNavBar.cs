3e3ab678f89e1e73757a5ff8fb521707d8f686d5
KostiaSA <savchenkov@buhta.ru>
2015-12-14 14:57:21 +0300

    bsAppNavBar

---
+++ Buhta4/Areas/BuhtaCore/CODE/BS-CONTROLS/BS-APPNAVBAR/bsAppNavBar.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Buhta
+{
+    public static partial class HtmlHelperExtensions
+    {
+
+        public static MvcHtmlString bsAppNavBar(this HtmlHelper helper, Action<bsAppNavBar> settings)
+        {
+            var tag = new bsAppNavBar(helper.ViewData.Model as BaseModel);
+            settings(tag);
+
+            (helper.ViewData.Model as BaseModel).Helper = helper;
+            return new MvcHtmlString(tag.GetHtml());
+        }
+
+    }
+
+    public class bsAppNavBar : bsControl
+    {
+        public bsAppNavBar(BaseModel model) : base(model) { }
+
+        List<bsControl> leftToolbar = new List<bsControl>();
+        List<bsControl> rightToolbar = new List<bsControl>();
+
+        public void AddButtonToRightToolbar(Action<bsAppNavBarButton> settings)
+        {
+            var button = new bsAppNavBarButton(Model);
+            button.NavBar = this;
+            settings(button);
+            rightToolbar.Add(button);
+        }
+
+        public void AddButtonToLeftToolbar(Action<bsAppNavBarButton> settings)
+        {
+            var button = new bsAppNavBarButton(Model);
+            button.NavBar = this;
+            settings(button);
+            leftToolbar.Add(button);
+        }
+
+        public override string GetHtml()
+        {
+            Html.Append("<nav id='" + UniqueId + "' " + GetAttrs() + " class='navbar navbar-default'>");
+            Html.Append("<div class='container' style='padding-left:0px;padding-right:0px'>");
+            Html.Append("<div class='collapse navbar-collapse'>");
+
+            Html.Append("<ul class='nav navbar-nav app-navbar'>");
+
+            foreach (var control in leftToolbar)
+            {
+                Html.Append("<li>");
+                Html.Append(control.GetHtml());
+                Html.Append("</li>");
+            }
+            Html.Append("</ul>");
+
+            Html.Append("<ul class='nav navbar-nav app-navbar pull-right'>");
+            foreach (var control in rightToolbar)
+            {
+                Html.Append("<li>");
+                Html.Append(control.GetHtml());
+                Html.Append("</li>");
+            }
+            Html.Append("</ul>");
+
+            Html.Append("</div>");
+            Html.Append("</div>");
+            Html.Append("</nav>");
+
+            return base.GetHtml();
+        }
+    }
+
+
+}
---
+++ Buhta4/Areas/BuhtaCore/CODE/BS-CONTROLS/BS-APPNAVBAR/bsAppNavBarButton.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buhta
+{
+    public static partial class HtmlHelperExtensions
+    {
+
+        public static MvcHtmlString bsAppNavBarButton(this HtmlHelper helper, Action<bsAppNavBarButton> settings)
+        {
+            var Settings = new bsAppNavBarButton(helper.ViewData.Model as BaseModel);
+            settings(Settings);
+
+            (helper.ViewData.Model as BaseModel).Helper = helper;
+            return new MvcHtmlString(Settings.GetHtml());
+        }
+
+    }
+
+
+    public class bsAppNavBarButton : bsControl
+    {
+        public bsAppNavBarButton(BaseModel model) : base(model) { }
+
+        public bool? Disabled;
+        public string Disabled_Bind;
+
+        public string Text = "";
+
+        public bsAppNavBar NavBar;
+
+        public void Bind_Text(string modelPropertyName)
+        {
+            AddBinder(new OneWayBinder<string>()
+            {
+                ModelPropertyName = modelPropertyName,
+                jsSetMethodName = "text"
+            });
+        }
+
+        public void Bind_Text(BinderGetMethod<string> getValueMethod)
+        {
+            AddBinder(new OneWayBinder<string>()
+            {
+                ModelGetMethod = getValueMethod,
+                jsSetMethodName = "text"
+            });
+        }
+
+        public void Bind_Disabled(string modelPropertyName)
+        {
+            AddBinder(new OneWayBinder<bool>()
+            {
+                ModelPropertyName = modelPropertyName,
+                jsSetMethodName = "prop",
+                jsSetPropertyName = "disabled"
+            });
+        }
+
+        public void Bind_Disabled(BinderGetMethod<bool> getValueMethod)
+        {
+            AddBinder(new OneWayBinder<bool>()
+            {
+                ModelGetMethod = getValueMethod,
+                jsSetMethodName = "prop",
+                jsSetPropertyName = "disabled"
+            });
+        }
+
+        public void Bind_OnClick(string modelEventMethodName)
+        {
+            AddBinder(new EventBinder()
+            {
+                ModelEventMethodName = modelEventMethodName,
+                jsEventName = "click"
+            });
+        }
+
+        public void Bind_OnClick(BinderEventMethod eventMethod)
+        {
+            AddBinder(new EventBinder()
+            {
+                ModelEventMethod = eventMethod,
+                jsEventName = "click"
+            });
+        }
+
+
+        public string Image;
+
+        //public string OnClick_Bind;
+
+        public BaseAction ClickAction;
+
+        public override string GetHtml()
+        {
+            if (ClickAction != null)
+            {
+                Script.AppendLine("tag.on('click',function(event){");
+                ClickAction.EmitJsCode(Script);
+                Script.AppendLine("});");
+
+            }
+
+            var imageHtml = "";
+            if (Image != null)
+                imageHtml = "<img src=" + new UrlHelper(HttpContext.Current.Request.RequestContext).Content(Image).AsJavaScript() + " width='30' height='30'/>";
+
+            Html.Append("<a id='" + UniqueId + "' " + GetAttrs() + " href=javascript:void(0)'>" + imageHtml + "<p>" + Text.AsHtmlEx() + "</p></a>");
+
+            return base.GetHtml();
+        }
+    }
+
+
+}
